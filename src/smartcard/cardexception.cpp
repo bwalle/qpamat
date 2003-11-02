346d59198527d4027e1bf3fe510ea8461ef5199d
bwalle <devnull@localhost>
2003-11-02 20:00:36 +0000

    Initial import

---
+++ src/smartcard/cardexception.cpp
@@ -0,0 +1,63 @@
+/*
+ * Id: $Id: cardexception.cpp,v 1.1 2003/11/02 20:00:36 bwalle Exp $
+ * -------------------------------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; You may only use 
+ * version 2 of the License, you have no option to use any other version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if 
+ * not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------------------------------- 
+ */
+#include <stdexcept>
+#include <string>
+
+#include "cardexception.h"
+
+// -------------------------------------------------------------------------------------------------
+CardException::CardException(const std::string& message)
+// -------------------------------------------------------------------------------------------------
+        : std::runtime_error(message), m_errorcode(NoError)
+{}
+
+
+// -------------------------------------------------------------------------------------------------
+CardException::CardException(ErrorCode errorcode)
+// -------------------------------------------------------------------------------------------------
+        : std::runtime_error(""), m_errorcode(errorcode)
+{}
+
+// -------------------------------------------------------------------------------------------------
+const char* CardException::what() const throw ()
+// -------------------------------------------------------------------------------------------------
+{
+    switch (m_errorcode)
+    {
+        case Invalid:
+            return "Invalid parameter or value.";
+        case CT:
+            return "Cardterminal Error. The terminal is temporary not accessible.";
+        case Transmission:
+            return "Transmission error.";
+        case Memory:
+            return "Memory assignment error.";
+        case HTSI:
+            return "Host Transport Service Interface error.";
+        default:
+            return std::runtime_error::what();
+    }
+}
+
+
+// -------------------------------------------------------------------------------------------------
+CardException::ErrorCode CardException::getErrorCode() const
+// -------------------------------------------------------------------------------------------------
+{
+    return m_errorcode;
+}
---
+++ src/smartcard/cardexception.h
@@ -0,0 +1,90 @@
+/*
+ * Id: $Id: cardexception.h,v 1.1 2003/11/02 20:00:36 bwalle Exp $
+ * -------------------------------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; You may only use 
+ * version 2 of the License, you have no option to use any other version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if 
+ * not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------------------------------- 
+ */
+#ifndef CARDEXCEPTION_H
+#define CARDEXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+
+#include "ctapi.h"
+
+/**
+ * This exception is thrown if an error occured while communicating with the smartcard
+ * terminal. It just encapsulates the error codes of CT-API in a programmer-friendly
+ * way.
+ * @ingroup smartcard
+ * @author Bernhard Walle
+ * @version $Revision: 1.1 $
+ * @date $Date: 2003/11/02 20:00:36 $
+ */
+class CardException : public std::runtime_error
+{
+    public:
+        
+        /** 
+         * Enumeration for CT-API errorcodes.
+         */
+        enum ErrorCode 
+        {
+            NoError      = 0,           /*!< No error, used for passing a message. */
+            Invalid      = ERR_INVALID, /*!< Invalid parameter or value. */ 
+            CardTerminal = ERR_CT,      /*!< Cardterminal Error. The terminal is temporary not
+                                             accessible (busy with other or internal processes).
+                                             The problem can be resolved by the application. */
+            Transmission = ERR_TRANS,   /*!< Transmission error. Transmission error due to
+                                             mechanical, electrical or protocol failures. Reset of
+                                             of cardterminal is necessary. */
+            Memory       = ERR_MEMORY,  /*!< Memory assignment error. A memory error occured (the
+                                             allocated buffer is too small for the returned data).*/
+            HTSI         = ERR_HTSI     /*!< Host Transport Service Interface error. Commonly re-
+                                             turned if the error is produced by the software layer
+                                             and not in the communication with the hardware. */
+        };
+            
+        /**
+         * Creates a new instance of the exception and includes the error message. This
+         * message is returned by the what() method. Consider using the other constructor
+         * which takes a 
+         * @param error the error message
+         */
+        CardException(const std::string& error);
+        
+        /**
+         * Creates a new instance of the exception and sets the error code. The error message
+         * is generated on the fly if requested.
+         */
+        CardException(ErrorCode errorcode);
+        
+        /**
+         * Returns the error message.
+         * @return the error message (in English)
+         */
+        virtual const char* what() const throw ();
+        
+        /**
+         * Returns the error code.
+         * @return the error code
+         */
+        ErrorCode getErrorCode() const;
+    
+    private:
+        ErrorCode m_errorcode;
+};
+
+
+#endif // CARDEXCEPTION_H
