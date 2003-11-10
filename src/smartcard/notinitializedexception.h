1c35351ab978e24c833c3d1deb977d1e8b8d6e1b
bwalle <devnull@localhost>
2003-11-10 23:17:40 +0000

    Added QString convenience function

--- src/smartcard/notinitializedexception.h
+++ src/smartcard/notinitializedexception.h
@@ -1,5 +1,5 @@
 /*
- * Id: $Id: notinitializedexception.h,v 1.1 2003/11/04 22:49:06 bwalle Exp $
+ * Id: $Id: notinitializedexception.h,v 1.2 2003/11/10 23:17:40 bwalle Exp $
  * -------------------------------------------------------------------------------------------------
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the 
@@ -26,9 +26,9 @@
 /**
  * Exception that is thrown if the object was not initalized using the init() method.
  * @ingroup smartcard
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @author Bernhard Walle
- * @date $Date: 2003/11/04 22:49:06 $
+ * @date $Date: 2003/11/10 23:17:40 $
  */
 class NotInitializedException : public std::runtime_error
 {
@@ -40,6 +40,15 @@ class NotInitializedException : public std::runtime_error
          * @param error the error message
          */
         NotInitializedException(const std::string& error) : std::runtime_error(error) { }
+        
+        
+        /**
+         * Creates a new instance of the exception and includes the error message. This
+         * message is returned by the what() method. It behaves exactly like the above
+         * constructor and is provided only for convenience.
+         * @param error the error message
+         */
+        NotInitializedException(const QString& error) : std::runtime_error(error.latin1()) { }
         
         /**
          * Deletes the object.
