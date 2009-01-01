65f340fe75d84f2583d3293f6cfb6d848d9b83ed
Bernhard Walle <bernhard.walle@gmx.de>
2009-01-01 17:57:38 +0100

    Add documentation for ProcessInfo

--- src/util/processinfo_unix.cpp
+++ src/util/processinfo_unix.cpp
@@ -1,16 +1,16 @@
 /*
- * This program is free software; you can redistribute it and/or modify it under the terms of the 
- * GNU General Public License as published by the Free Software Foundation; You may only use 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; You may only use
  * version 2 of the License, you have no option to use any other version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
  * the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program; if 
+ * You should have received a copy of the GNU General Public License along with this program; if
  * not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * ------------------------------------------------------------------------------------------------- 
+ * -------------------------------------------------------------------------------------------------
  */
 #include <unistd.h>
 #include <signal.h>
@@ -18,12 +18,31 @@
 
 #include "processinfo.h"
 
+/*!
+    \class ProcessInfo
 
+    \brief Information about the current process
+
+    \ingroup util
+    \author Bernhard Walle
+*/
+
+/*!
+    Checks if the specified process is currently running.
+
+    \param pid the PID of the process that should be checked
+    \return \c true if the process is running, \c false otherwise.
+*/
 bool ProcessInfo::isProcessRunning(q_pid_t pid)
 {
     return !kill(pid, 0);
 }
 
+/*!
+    Returns the PID (process ID) of the process that is currently active.
+
+    \return the PID of the current proces.
+*/
 q_pid_t ProcessInfo::getCurrentPid()
 {
     return getpid();
