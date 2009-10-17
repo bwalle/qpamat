a2e285e7f3494391bd66cf3e57371a7ac0570aa1
Bernhard Walle <bernhard@bwalle.de>
2009-10-17 19:26:34 +0200

    Use Q_FUNC_INFO for CURRENT_FUNCTION
    Use Q_FUNC_INFO for CURRENT_FUNCTION instead of defining the value for all
    compiler flavours ourselves.

--- src/global.h
+++ src/global.h
@@ -84,24 +84,6 @@ typedef Q3ValueVector<QChar>     QCharVector;
  */
 #define UNUSED(param) (void)param;
 
-#ifndef DOXYGEN
-#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600))
-# define _CURRENT_FUNCTION __PRETTY_FUNCTION__
-#elif defined(__DMC__) && (__DMC__ >= 0x810)
-# define _CURRENT_FUNCTION __PRETTY_FUNCTION__
-#elif defined(__FUNCSIG__)
-# define _CURRENT_FUNCTION __FUNCSIG__
-#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-# define _CURRENT_FUNCTION __FUNCTION__
-#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-# define _CURRENT_FUNCTION __FUNC__
-#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-# define _CURRENT_FUNCTION __func__
-#else
-# define _CURRENT_FUNCTION "(unknown)"
-#endif
-#endif // DOXYGEN
-
 /**
  * Macro for the current function with signature included in brackets. Example:
  *
@@ -117,7 +99,7 @@ typedef Q3ValueVector<QChar>     QCharVector;
  * @endcode
  */
 #define CURRENT_FUNCTION \
-        "[" << _CURRENT_FUNCTION << "]"
+        "[" << Q_FUNC_INFO << "]"
 
 #endif // GLOBAL_H
 
