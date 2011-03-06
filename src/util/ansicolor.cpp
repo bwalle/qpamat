0c3c194147935cb18c979c8e70ee12b88b99a527
Bernhard Walle <bernhard@bwalle.de>
2011-03-06 11:12:35 +0100

    Replace "\e" with "\033"
    This is ANSI C++.
    
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- src/util/ansicolor.cpp
+++ src/util/ansicolor.cpp
@@ -43,57 +43,57 @@
  */
 
 /// Resets all color codes.
-const char *AnsiColor::NORMAL           = "\e[0m";
+const char *AnsiColor::NORMAL           = "\033[0m";
 
 /// Enables bold text
-const char *AnsiColor::BOLD             = "\e[1m";
+const char *AnsiColor::BOLD             = "\033[1m";
 
 /// Black color (not bold)
-const char *AnsiColor::BLACK            = "\e[30m";
+const char *AnsiColor::BLACK            = "\033[30m";
 
 /// Red color (not bold)
-const char *AnsiColor::RED              = "\e[31m";
+const char *AnsiColor::RED              = "\033[31m";
 
 /// Green color (not bold)
-const char *AnsiColor::GREEN            = "\e[32m";
+const char *AnsiColor::GREEN            = "\033[32m";
 
 /// Yellow color (not bold)
-const char *AnsiColor::YELLOW           = "\e[33m";
+const char *AnsiColor::YELLOW           = "\033[33m";
 
 /// Blue color (not bold)
-const char *AnsiColor::BLUE             = "\e[34m";
+const char *AnsiColor::BLUE             = "\033[34m";
 
 /// Magenta color (not bold)
-const char *AnsiColor::MAGENTA          = "\e[35m";
+const char *AnsiColor::MAGENTA          = "\033[35m";
 
 /// Cyan color (not bold)
-const char *AnsiColor::CYAN             = "\e[36m";
+const char *AnsiColor::CYAN             = "\033[36m";
 
 /// White color (not bold)
-const char *AnsiColor::WHITE            = "\e[37m";
+const char *AnsiColor::WHITE            = "\033[37m";
 
 /// Black color (bold)
-const char *AnsiColor::BRIGHT_BLACK     = "\e[30;1m";
+const char *AnsiColor::BRIGHT_BLACK     = "\033[30;1m";
 
 /// Red color (bold)
-const char *AnsiColor::BRIGHT_RED       = "\e[31;1m";
+const char *AnsiColor::BRIGHT_RED       = "\033[31;1m";
 
 /// Green color (bold)
-const char *AnsiColor::BRIGHT_GREEN     = "\e[32;1m";
+const char *AnsiColor::BRIGHT_GREEN     = "\033[32;1m";
 
 /// Yellow color (bold)
-const char *AnsiColor::BRIGHT_YELLOW    = "\e[33;1m";
+const char *AnsiColor::BRIGHT_YELLOW    = "\033[33;1m";
 
 /// Blue color (bold)
-const char *AnsiColor::BRIGHT_BLUE      = "\e[34;1m";
+const char *AnsiColor::BRIGHT_BLUE      = "\033[34;1m";
 
 /// Magenta color (bold)
-const char *AnsiColor::BRIGHT_MAGENTA   = "\e[35;1m";
+const char *AnsiColor::BRIGHT_MAGENTA   = "\033[35;1m";
 
 /// Cyan color (bold)
-const char *AnsiColor::BRIGHT_CYAN      = "\e[36;1m";
+const char *AnsiColor::BRIGHT_CYAN      = "\033[36;1m";
 
 /// White color (bold)
-const char *AnsiColor::BRIGHT_WHITE     = "\e[37;1m";
+const char *AnsiColor::BRIGHT_WHITE     = "\033[37;1m";
 
 // vim: set sw=4 ts=4 et: :tabSize=4:indentSize=4:maxLineLen=100:
